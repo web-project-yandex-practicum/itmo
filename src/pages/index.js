387a84e910f77be4d19641315d5f9ae5949a1e53
Александр Кучеренко <g28xyz@ya.ru>
2022-03-30 14:16:54 +0700

    feat: добавлен функционал для кнопки menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,7 @@
 import "./index.css";
+import { headerMenuButton, headerMenuList } from "../utils/constants";
+
+headerMenuButton.addEventListener("click", (e) => {
+  e.target.classList.toggle("header__menu-button_active");
+  headerMenuList.classList.toggle("header__menu-list_active");
+});
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const headerMenuButton = document.querySelector(".header__menu-button");
+const headerMenuList = document.querySelector(".header__menu-list");
+
+export { headerMenuButton, headerMenuList };
